d96ad619daa26b134df9c0c44fa171d5a55f4572
setadmin <setadmin@DESKTOP-QJ34O9U>
2022-02-25 21:56:12 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+        //CRUD
+        //Type Name();
+        List<Category> List();
+
+        //insert 
+        void Insert(Category p);
+
+        //Update
+        void Update(Category p);
+
+        //Delete
+        void Delete(Category p);
+    }
+
+}
---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        //CRUD
+        //Type Name();
+        List<T> List();
+
+        //insert 
+        void Insert(T p);
+
+        //Update
+        void Update(T p);
+
+        //Delete
+        void Delete(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
